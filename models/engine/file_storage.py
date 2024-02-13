cf84ea39ec85461fb10332f82d4235deb41c7e1b
Munoru101 <dennismunoru@gmail.com>
2024-02-13 12:52:45 +0300

    Integrated models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,64 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-import json
-import os
-import self
-import models
-
-
-class FileStorage:
-    """Serializes instances to a JSOn file
-    and deserializes Json file to instances
-    """
-
-
-__file_path = "file.json"
-__objects = {}
-
-
-def all():
-    """Returns the dictionary_objects"""
-    return self.__objects
-
-
-def new(obj):
-    """Sets in __objects the obj with key <obj class name>.id
-    :rtype: object
-    """
-
-
-key = "{}.{}".format(obj.__class__.__name__, obj.id)
-self.__objects[key] = __objects
-
-
-def save():
-    """Serializes __objects to the JSON file (path: __file_path)."""
-
-
-serialized_objects = {}
-for key, obj in self.__objects.items():
-    serialized_objects[key] = obj.to_dict()
-    with open(self.__file_path, 'w') as file:
-        json.dump(serialized_objects, file)
-
-
-def reload():
-    """Deserializes the JSON file to __objects
-    (only if the JSON file (__file_path) exists;
-      otherwise, do nothing. If the file does not exist,
-      no exception should be raised).
-      """
-    if os.path.exists(self.__file_path):
-        with open(self.__file_path, 'r') as file:
-            data = json.load(file)
-            for key, value in data.items():
-                class_name, obj_id = key.split('.')
-            module = __import__('models')
-            class_ = getattr(module, class_name)
-            instance = class_(**value)
-            self.__objects[key] = instance
-=======
-
 """
 Defines the FileStorage class.
 """
@@ -124,4 +64,3 @@ class FileStorage:
                         cls.__objects[key] = obj
         except FileNotFoundError:
             pass
->>>>>>> ccc0d1c6b2deb7c2cff24f19deddd62a4bd5e1ff
